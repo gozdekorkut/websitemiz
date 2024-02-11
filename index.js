dbd4f32ece597e6acb3f01f44dfce83afd862dde
gozdekorkut <gozdeekorkutt@gmail.com>
2024-02-11 03:06:29 +0300

    kurabiyeden önce

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 let uyuyan = document.getElementById('uyuyan')
 
-uyuyan.addEventListener('click', tiklama)
+uyuyan.addEventListener('click', rahatsızEtmeyin)
 
-function tiklama() {
+function rahatsızEtmeyin() {
 
-    alert('Kirpi uyuyor, lütfen rahatsız etmeyin!')
+    alert('Kirpi uyuyor, lütfen rahatsız etme!')
+}
+
+let tetikte = document.getElementById('tetikte')
+
+tetikte.addEventListener('click', kurabiye)
+
+function kurabiye() {
+
+    alert('Yoksa o bir kurabiye mi?!')
+}
+
+let yürüyüş= document.getElementById('yürüyüş')
+
+yürüyüş.addEventListener('click', kirpiyeYardım)
+
+function kirpiyeYardım() {
+
+    alert('Kirpinin karnı acıkmaya başladı, ona kurabiyesini bulması için yardım eder misin?')
 }
